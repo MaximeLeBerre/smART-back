52ea3cc4c8ae4c7192a9cba65cab0cf6ee6a458a
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-02-03 11:47:08 +0100

    works route update

--- routes/works.js
+++ routes/works.js
@@ -3,9 +3,21 @@ const connection = require("../config");
 
 router.get("/", async (req, res, next) =>{
   try{
-    const [works] = await connection.query('SELECT * FROM work');
+    const [works] = await connection.query(`
+    SELECT 
+    w.*,
+    a.name AS artistName,
+    t.name AS themeName,
+    k.name AS kindName
+    FROM 
+    work AS w 
+    JOIN artist AS a ON w.artist_id=a.id
+    JOIN theme AS t ON w.theme_id=t.id
+    JOIN kind AS k ON w.kind_id=k.id
+    `);
     res.status(200).json(works)
   }catch (error) {
+    console.log(error.message)
     next(error)
   }
 });
